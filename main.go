faf49f3dd658b922bf4283a2e50ae78547b0c767
Isaac Wanger <teryima127@gmail.com>
2022-12-01 21:29:56 +0100

    created a random guess number game

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	const (
+		maxTurns = 5
+		usage = `Welcome to the Luck Number Game!
+		The game will pick %d random numbers.
+		Your mission is to guess one of those numbers.
+		The greater your numbers are, the harder it gets.
+
+		Wanna play?
+	`
+
+	)
+	
+	rand.Seed(time.Now().UnixNano())
+
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Printf(usage, maxTurns)
+		return
+	}
+
+
+	guess, err := strconv.Atoi(args[0])
+	if err != nil {
+		fmt.Println("Not a number")
+		return
+	}
+
+	if guess < 0 {
+		fmt.Println("Pick a positive number")
+		return
+	}
+
+	for turn := 0; turn < maxTurns; turn++ {
+		n := rand.Intn(guess + 1)
+
+		if n == guess {
+			fmt.Println("🏆 YOU WON!!!")
+			return
+		} 
+	}
+	fmt.Println("🎴 YOU LOST... Try again")
+	
+}
